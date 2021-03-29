5bcd507d32977fb151a14d2160493cadea9ccdd0
Neelam618 <neelamchavan410@gmail.com>
2021-03-29 17:37:31 +0530

    used args for MaterailUI button

--- src/components/MaterialUIButton/MaterialUIButton.stories.js
+++ src/components/MaterialUIButton/MaterialUIButton.stories.js
@@ -4,8 +4,30 @@ import { Button } from '@material-ui/core';
 
 export default {
     title: 'Material UI/Button',
-    component: Button
+    component: Button,
+    args: {
+        variant: 'contained'
+    }
 }
 
-export const Primary = () => <Button variant="contained" color ='primary'>Primary</Button>
-export const Secondary = () => <Button variant="contained" color ='secondary'>Secondary</Button>
+// export const Primary = () => <Button variant="contained" color ='primary'>Primary</Button>
+// export const Secondary = () => <Button variant="contained" color ='secondary'>Secondary</Button>
+
+// Using args
+
+const Template = args => <Button {...args}/>
+
+export const Primary = Template.bind({})
+Primary.args = {
+    // variant: 'contained',
+    color: 'primary',
+    children: 'Primary'
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+    // variant: 'contained',
+    color: 'secondary',
+    children: 'Secondary'
+}
+
