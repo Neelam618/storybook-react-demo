457ce4be9a4bcf712ff3dc0e8620f2db7a97b973
Neelam618 <neelamchavan410@gmail.com>
2021-03-29 14:55:10 +0530

    rename story

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -10,5 +10,5 @@ export const Small = () => <Input size ='small' placeholder='Small size'/>
 export const Medium = () => <Input size ='medium' placeholder='Medium size'/>
 export const Large = () => <Input size ='large' placeholder='Large size' />
 
-// Rename story
+// Rename story Small
 Small.storyName = 'Small Input';
