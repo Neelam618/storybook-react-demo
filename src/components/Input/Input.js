8c943b32b686a11621cd8fefa272b0a5a8e23692
Neelam618 <neelamchavan410@gmail.com>
2021-03-29 11:47:09 +0530

    added input component and input.js content

---
+++ src/components/Input/Input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Input.css';
+
+
+function Input(props) {
+    const {size = 'medium', ...rest} = props
+    return (
+        <Input className={`input ${size}`} {...rest}>
+            {children}
+        </Input>
+    )
+}
+
+export default Input;
