263e6fb9d0dee65685650c5d8f7ba1e311c18f80
Neelam618 <neelamchavan410@gmail.com>
2021-03-29 15:56:19 +0530

    added decorator Center which wraps the component

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import Center from '../Center/Center';
 
 export default {
     title: 'Form/Button',
@@ -9,14 +10,15 @@ export default {
     }
 }
 
-export const Primary = () => <Button variant ='primary'>Primary</Button>
-export const Secondary = () => <Button variant ='secondary'>Secondary</Button>
-export const Success = () => <Button variant ='success'>Success</Button>
-export const Danger = () => <Button variant ='danger'>Danger</Button>
+// Stories: first approach
+export const Primary = () => <Center><Button variant ='primary'>Primary</Button></Center>
+export const Secondary = () => <Center><Button variant ='secondary'>Secondary</Button></Center>
+export const Success = () => <Center><Button variant ='success'>Success</Button></Center>
+export const Danger = () => <Center><Button variant ='danger'>Danger</Button></Center>
 
 // Second approach: By passing arguments to button component
 
-const Template = args => <Button {...args} />
+const Template = args => <Center><Button {...args} /></Center>
 
 export const PrimaryA = Template.bind({})
 PrimaryA.args = {
